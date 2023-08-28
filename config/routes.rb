1bf383151cf0a6c21c049c1dcee0e896a701ba2d
codemi <remy@codemi.de>
2023-08-28 17:11:29 +0200

    Implemented index view for all AI tools per category

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root "pages#show"
-  #resources :ai_tools,only: [:index,:show,:create, :new]
   resources :ai_tools
+  resources :category_pages, only:  [:show, :index]
 end
